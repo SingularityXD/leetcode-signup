e2193455ff89553ae415dae67c53cccc51aecb91
huanghuiqi <huanghuiqi@cmbi.com.hk>
2020-10-13 23:05:51 +0800

    sign:2020-10-13 23:04:46

--- js/20.js
+++ js/20.js
@@ -30,3 +30,30 @@ const isValid = function(s) {
     }
     return stack.length === 0
 };
+
+const isValid = function(s) {
+    if (s.length % 2 !== 0) {
+        return false
+    }
+    const stack = []
+    const map = new Map()
+    map.set('{','}')
+    map.set( '[',']')
+    map.set( '(',')')
+    const lefts = Array.from(map.keys())
+    for (let char of s) {
+        if (lefts.includes(char)) {
+            stack.push(char)
+        } else if (stack.length === 0) {
+            return false
+        } else {
+            const top = stack[stack.length - 1]
+            if (map.get(top) === char) {
+                stack.pop()
+            } else {
+                return false
+            }
+        }
+    }
+    return stack.length === 0
+};
