543b8b1d79ad5a9cd2921b12c718659848b052e0
SingularityXD <xtonylaw@gmail.com>
2020-10-13 23:19:34 +0800

    sign:2020-10-13 23:19:30

--- js/1.js
+++ js/1.js
@@ -14,3 +14,16 @@ const twoSum = function(nums, target) {
         diffs.set(nums[i],i)
     }
 };
+
+const twoSum = function(nums, target) {
+    const map = new Map()
+    for(let i = 0;i<nums.length;i++){
+        const item = nums[i]
+        const diff = target - item
+        if(map.has(diff)){
+          return [map.get(diff),i]
+        }else{
+         map.set(item,i) 
+        }
+    }
+  };
